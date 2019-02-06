57bff30595533cf105382549c92a17b019f50e1b
kkhgm <koki@higamimitsuterunoMacBook-Air.local>
2019-02-06 22:30:37 +0900

    Create CreateMehtod

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,14 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+    Message.create(messages_params)
+  end
+
+  private
+  def messages_params
+    params.require(:message).permit(:content, :image, :group_id, :user_id)
+  end
+
 end
